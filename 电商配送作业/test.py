075ce6eb270987bb8e9c4ac0172c975749b1c6cd
WaiHouer <739488321@qq.com>
2021-10-16 21:47:19 +0800

    10.16.10:39

--- 电商配送作业/test.py
+++ 电商配送作业/test.py
@@ -1,10 +1,15 @@
+import random
 
-
-'''
-
-
-
-wadwdawidjowi jj wap
-
-'''
-print('敖德萨大所大大所')
+print(f'佩戴口罩1-{random.randint(20,31)}')
+x = random.randint(23,31)
+print(f'隔离1-{x}')
+if random.randint(0,10) >= 5:
+    print(f'社交距离5-{random.randint(1,30)}')
+else:
+    print(f'社交距离4-{random.randint(20,31)}')
+if random.randint(0,10) >= 3:
+    print(f'核酸检测2-{random.randint(1,15)}')
+else:
+    print(f'核酸检测1-{random.randint(25,31)}')
+print(f'疫苗接种12-{random.randint(10,31)}')
+print(f'把控入境关口1-{x}')
